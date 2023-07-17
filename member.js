e68b1d821c96fa1f2d85f8bc91cc14c024a93764
Cris Fallas <cristobalfallascr@hotmail.com>
2023-07-17 20:28:12 +0000

    copilot seocnd commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var member = this;
+    member.skills = [];
+    member.addSkill = function (skill) {
+        member.skills.push(skill);
+    };
+    member.removeSkill = function (skill) {
+        var index = member.skills.indexOf(skill);
+        if (index > -1) {
+            member.skills.splice(index, 1);
+        }
+    };
+}
