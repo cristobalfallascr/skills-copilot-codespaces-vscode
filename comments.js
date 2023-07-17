a7818a7e0c021e7ea988f5cc9534f3dfa5872244
Cris Fallas <cristobalfallascr@hotmail.com>
2023-07-17 21:47:22 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,14 +1,44 @@
 //create web server
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
 
-//create a route
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+//add static files
+app.use(express.static('public'));
 
-//start server
+//add body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//add database
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('comment.json');
+const db = low(adapter);
+
+//add database default data
+db.defaults({ comment: [] }).write();
+
+//add pug
+app.set('view engine', 'pug');
+
+//add route
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+//add post route
+app.post('/comment', (req, res) => {
+    db.get('comment').push(req.body).write();
+    res.redirect('/');
+})
+
+//add comment route
+app.get('/comment', (req, res) => {
+    res.render('comment', { comment: db.get('comment').value() });
+})
+
+//server listen
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+    console.log('server running on port ' + port);
+})
